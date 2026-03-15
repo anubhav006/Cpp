6d5145486f304c33538bccd080ee7a6eeaab3619
anubhav006 <anubhavprajapati09@gmail.com>
2026-03-15 22:05:49 +0530

    done

---
+++ operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int num1=7;
+    int num2=8;
+
+    cout<<num1+num2<<endl;
+    cout<<num1-num2<<endl;
+    cout<<num1%num2<<endl;
+    cout<<num1/num2<<endl;
+
+    return 0;
+}
